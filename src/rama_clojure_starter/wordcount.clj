466f50fd4632ac14d9b038aaa5a739eecc8783ef
Jason Jackson <jasonjckn@gmail.com>
2023-11-10 19:23:44 -0800

    todo(WIP): write commit message

--- src/rama_clojure_starter/wordcount.clj
+++ src/rama_clojure_starter/wordcount.clj
@@ -10,67 +10,45 @@
 (defmodule MyModule [R R-topo]
   (declare-depot R *depot :random)
   (let [s (stream-topology R-topo "wc-topology")]
-    (declare-pstate s $$word->count {String Long})
+    (declare-pstate s $$word->count {clojure.lang.Keyword Long} {:global? true})
 
     (<<sources s
-      (source> *depot :> *sentence)
-      (str/split *sentence #"\s+" :> *words)
-      (r.ops/explode *words :> *word)
+      (source> *depot :> *word)
+      ;; (str/split *sentence #"\s+" :> *words)
+      ;; (r.ops/explode *words :> *word)
       (|hash *word)
       (+compound $$word->count {*word (r.aggs/+count)} )
       (printf "'%s', " *word))))
 
-(defn -main [& args ]
+
+
+(defn -main [& _]
   (defonce ipc (r.test/create-ipc))
-  (try
-    (r.test/destroy-module! ipc (get-module-name MyModule))
-    (catch Exception _))
+
+  (try (r.test/destroy-module! ipc (get-module-name MyModule))
+       (catch Exception _))
 
 
   (r.test/launch-module! ipc MyModule {:tasks 2 :threads 2})
   (def depot (foreign-depot ipc (get-module-name MyModule) "*depot"))
-
   (def $$word->count (foreign-pstate ipc (get-module-name MyModule) "$$word->count"))
 
 
   (println "\n--------------------------")
-  (doseq [w [:a :a :a :b :c]]
-    (foreign-append! depot w))
+  (foreign-append! depot :a)
+  (foreign-append! depot :a)
+  (foreign-append! depot :a)
+  (foreign-append! depot :b)
+  (foreign-append! depot :c)
+  ;; (doseq [w [:a :a :a :b :c]]
+  ;;   (foreign-append! depot w))
 
-  (pr (foreign-select :a $$word->count))
+  ;; (pr (foreign-select :a $$word->count))
 
 
 
-
+  ;; (Thread/sleep 10000)
+  ;; (System/exit 0)
 
 
   )
-
-#_ (defonce ipc (r.test/create-ipc))
-
-#_ (do
-     (try
-       (r.test/destroy-module! ipc (get-module-name MyModule))
-       (catch Exception _))
-
-
-     (r.test/launch-module! ipc MyModule {:tasks 2 :threads 2})
-     (def depot (foreign-depot ipc (get-module-name MyModule) "*depot"))
-
-     (def $$word->count (foreign-pstate ipc (get-module-name MyModule) "$$word->count"))
-
-
-
-
-
-     (println "\n--------------------------")
-     (doseq [w [:a :a :a :b :c]]
-       (foreign-append! depot w))
-
-     (pr (foreign-select :a $$word->count))
-
-
-
-
-
-     )
